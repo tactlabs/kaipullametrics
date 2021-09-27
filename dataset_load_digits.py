2a9493944346c5230fadf3fb17c5a1e272960462
DhanyaB-26 <dhanyababu2003@gmail.com>
2021-09-27 21:31:44 +0530

    load_digits

---
+++ dataset_load_digits.py
@@ -0,0 +1,16 @@
+from prettymetrics.supervised import LazyClassifier
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+
+data = load_digits()
+X = data.data
+y= data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.5,random_state =123)
+
+clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
+models,predictions = clf.fit(X_train, X_test, y_train, y_test)
+
+print(models)
+
+# print(y)
