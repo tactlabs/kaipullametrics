ee96b13d2411d6475e78076bcc224783ee0e2ae1
ishita <ishitagops12@gmail.com>
2021-09-28 20:24:00 +0530

    classifers and regressors for drug,inurance and star datasets

---
+++ examples/diabetes_regressor.py
@@ -0,0 +1,17 @@
+import prettymetrics
+
+from prettymetrics.supervised import LazyRegressor
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+
+data = load_diabetes()
+X = data.data
+y= data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.2,random_state =123)
+
+reg = LazyRegressor(verbose=0,ignore_warnings=True, custom_metric=None)
+models,predictions = reg.fit(X_train, X_test, y_train, y_test)
+print(models)
+
+
