d459ef619557f7c2f4f28ed02eb6ecf973e50b90
Raja CSP Raman <raja.csp@gmail.com>
2021-10-11 21:47:21 -0400

    Code cleanup

--- examples/iris_classifier.py
+++ examples/iris_classifier.py
@@ -2,13 +2,13 @@ from prettymetrics.clf import Classifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
-data = load_iris()
-X = data.data
-y= data.target
+data                                = load_iris()
+X                                   = data.data
+y                                   = data.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.5,random_state =123)
+X_train, X_test, y_train, y_test    = train_test_split(X, y, test_size = .5, random_state = 123)
 
-clf = Classifier(verbose=0,ignore_warnings=True, custom_metric=None)
-models,predictions = clf.fit(X_train, X_test, y_train, y_test)
+clf                                 = Classifier(verbose = 0, ignore_warnings = True, custom_metric = None)
+models,predictions                  = clf.fit(X_train, X_test, y_train, y_test)
 
 print(models)
