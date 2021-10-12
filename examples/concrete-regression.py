e9785bd6579d6b605aa7eb6737e3dda66aa1f860
Raja CSP Raman <raja.csp@gmail.com>
2021-10-12 06:56:18 -0400

    Concrete regression sample updated

---
+++ examples/concrete-regression.py
@@ -0,0 +1,22 @@
+
+
+import pandas as pd
+from prettymetrics.reg import Regressor
+from sklearn import datasets
+from sklearn.utils import shuffle
+import numpy as np
+
+df = pd.read_csv('examples/dataset/concrete.csv')
+
+X, y    = shuffle(df[df.columns[:-1]], df['strength'], random_state = 13)
+X       = X.astype(np.float32)
+
+offset  = int(X.shape[0] * 0.9)
+
+X_train, y_train    = X[:offset], y[:offset]
+X_test, y_test      = X[offset:], y[offset:]
+
+reg                 = Regressor(verbose=0, ignore_warnings=False, custom_metric=None)
+models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+
+print(models)
